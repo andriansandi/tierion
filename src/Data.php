9b7a8ce8e1bcbe1ac3b10a39800a02ab861ba649
Sandi Andrian <andrian.sandi@gmail.com>
2016-09-21 10:10:36 +0700

    Initial Commit

---
+++ src/Data.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Tierion Data API
+ *
+ * @author 		Sandi Andrian (andrian.sandi@gmail.com)
+ * @since   	Sep 21, 2016
+ * @version 	0.1
+ **/
+
+namespace Tierion;
+
+class Data {
+
+	public static function create() 
+	{
+		return 'data create';
+	}
+
+}
